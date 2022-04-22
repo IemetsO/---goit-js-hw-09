eda0cc66f1dfc2501a7bc0bf9204234894b51bf7
IemetsO <oksana.v.iemets@gmail.com>
2022-04-22 15:58:22 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,31 @@
-function createPromise(position, delay) {
+const formEl = document.querySelector('.form');
+                
+const createPromise = (position, delay) => { 
+  return new Promise ((resolve, reject) => {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+  setTimeout(() => {
+    if (shouldResolve) {
+    resolve ({position, delay})
+    } else {
+    reject ({position, delay})}
+  }, delay)})
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });}
+  
+formEl.addEventListener('submit', ((e) => {
+  e.preventDefault()
+  const amount = formEl.elements[2].value; 
+  const delay = formEl.elements[0].value;
+  const step = formEl.elements[1].value;
+  createPromise (1, `${delay}` )
+  for (let i = 0; i < amount; i +=1){
+  createPromise(`${i+2}`, `${step}`)}
+}))
+
+
+
+
