c3faf73915b9b8c6b1c088c14f237c1fcefbae02
IemetsO <oksana.v.iemets@gmail.com>
2022-04-24 13:43:54 +0300

    fixed promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,31 +1,38 @@
+import Notiflix from 'notiflix';
 const formEl = document.querySelector('.form');
-                
-const createPromise = (position, delay) => { 
-  return new Promise ((resolve, reject) => {
-  const shouldResolve = Math.random() > 0.3;
-  setTimeout(() => {
-    if (shouldResolve) {
-    resolve ({position, delay})
-    } else {
-    reject ({position, delay})}
-  }, delay)})
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+
+const createPromise = (position, delay) => {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
   })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });}
-  
-formEl.addEventListener('submit', ((e) => {
-  e.preventDefault()
-  const amount = formEl.elements[2].value; 
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+};
+
+formEl.addEventListener('submit', e => {
+  e.preventDefault();
+  const amount = formEl.elements[2].value;
   const delay = formEl.elements[0].value;
   const step = formEl.elements[1].value;
-  createPromise (1, `${delay}` )
-  for (let i = 0; i < amount; i +=1){
-  createPromise(`${i+2}`, `${step}`)}
-}))
 
-
-
-
+  if (amount < 0 || delay < 0 || step < 0) {
+    Notiflix.Notify.warning('Значення має бути вище 0');
+    return;
+  } else {
+    createPromise(1, `${delay}`);
+    for (let i = 0; i < amount; i += 1) {
+      createPromise(`${i + 2}`, `${step * (i + 1)}`);
+    }
+  }
+});
